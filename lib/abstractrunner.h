1572476881a1380d3f07361d15d620e3b82a1050
Aaron Seigo <aseigo@kde.org>
2014-01-23 13:49:10 +0100

    ++apidox;

--- lib/abstractrunner.h
+++ lib/abstractrunner.h
@@ -37,23 +37,99 @@ public:
     AbstractRunner(QObject *parent);
     ~AbstractRunner();
 
+    /**
+     * The unique internal ID for this runner. This is retrieved
+     * from the plugin's metadata.
+     */
     QString id() const;
-    void setId(const QString &newId);
 
+    /**
+     * @return a new RunnerSessionData object for use in this session
+     *
+     * Runners which need to set up, access and/or store data members
+     * or other resources that should span an entire query session
+     * may reimplement this method to return a RunnerSessionData
+     * subclass.
+     *
+     * If not reimplemented, a generic RunnerSessionData object will
+     * be created on behalf of the runner.
+     *
+     * A new object must be returned every time this method is called
+     * and ownership of the resulting RunnerSessionData object is
+     * given to the caller. Runners must not manage the lifecycle of
+     * this object, e.g. by caching it internally.
+     */
     virtual RunnerSessionData *createSessionData();
 
+    /**
+     * This method will start a match. A number of pre-match
+     * checks are done and if they pass then the runner's implementation
+     * of @see match is called.
+     *
+     * @param sessionData the RunnerSessionData object from the query
+     * session for this runner. This object must have come from an
+     * earlier call to @see createSessionData.
+     * @param context the current search query data
+     */
     void startMatch(RunnerSessionData *sessionData, const QueryContext &context);
+
+    /**
+     * When a match is to be exec'd, this method can be called.
+     * A number of pre-exec checks are done and if they pass then
+     * the runner's implementation of @see exec is called
+     */
     bool startExec(const QueryMatch &match);
 
+    /**
+     * Sets the minimum query length accepted by this runner
+     * Used by pre-match checks in @see startMatch, for instance.
+     *
+     * @param legnth the number of characters a query term must have
+     * minimally to be of interest to the runner
+     */
     void setMinQueryLength(uint length);
+
+    /**
+     * @return the  number of characters a query term must have
+     * minimally to be of interest to the runner. Defaults to 3.
+     */
     int minQueryLength() const;
 
-
 protected:
+    /**
+     * Called when a match is to be made.
+     * @param sessionData this is the RunnerSessionData created by
+     * the runner (or on behalf of it) when the match session was
+     * initiated. Guaranteed to never be null; must not be deleted.
+     * @param context this object contains the query text itself and
+     * additional metadata. @see QueryContext
+     */
     virtual void match(RunnerSessionData *sessionData, const QueryContext &context);
+
+    /**
+     * Called when a match is to be executed. What that means
+     * precisely is up to the runner. The match is guaranteed
+     * to have been issued by this runner and be valid.
+     *
+     * If a runner does not reimplement this method, then if the
+     * userData() is valid, it will be placed on the clipboard when
+     * exec'd. A runner may also place a match's contents on
+     * the clipboard by calling @see QueryMatch::sendUserDataToClipboard()
+     *
+     * Note: QueryMatch objects which are marked as search terms
+     * will not be passed to this method
+     */
     virtual bool exec(const QueryMatch &match);
 
 private:
+    friend class RunnerFactory;
+    /**
+     * Allows setting the internal ID for the runner. This should
+     * not be called by the runner itself, but rather will be set
+     * for it on plugin load.
+     */
+    void setId(const QString &newId);
+
     class Private;
     Private * const d;
 };
--- lib/querymatch.h
+++ lib/querymatch.h
@@ -50,13 +50,15 @@ public:
     RunnerManager::MatchSource source() const;
 
     /**
-     * Sets if this match is a search term itself; when run
-     * it will replace the current query term with its data()
+     * Sets if this match is a search term itself; when exec'd
+     * it will replace the current query term with its data().toString()
+     *
+     * @param searchTerm true if this is a search term, false if not
      */
     void setIsSearchTerm(bool searchTerm);
 
     /**
-     * Returns whether or not this search term represents a
+     * @return whether or not this match represents a
      * search query term itself. Default is false
      */
     bool isSearchTerm() const;
@@ -64,21 +66,45 @@ public:
     /**
      * User data is what ends up on e.g. the clipboard for the user to
      * later reference
+     *
+     * @param data the user data to associate with the match
      */
     void setUserData(const QVariant &data);
+
+    /**
+     * @return the user data, if any, for this match
+     */
     QVariant userData() const;
 
     /**
      * Data is an internal notation for the runner to use in conjuction
      * with this match
+     *
+     * @param data the data to associate with the match
      */
     void setData(const QVariant &data);
+
+    /**
+     * @return the data, if any, for this match
+     */
     QVariant data() const;
 
+    /**
+     * Sets the precision of this match. @see RunnerManager
+     *
+     * @param precision the precision
+     */
     void setPrecision(RunnerManager::MatchPrecision precision);
+
+    /**
+     * @return the precision of this match, e.g. ExactMatch
+     */
     RunnerManager::MatchPrecision precision() const;
 
-
+    /**
+     * @return a pointer to the runner that created this match. May return
+     * a null pointer if the match is invalid or the runner has been deleted.
+     */
     AbstractRunner *runner() const;
 
     /**
